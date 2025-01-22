e4f68892123dd0a86bfe6153268883f63819b5f3
jpontel <jpontel2002@gmail.com>
2025-01-22 13:43:05 -0300

    implement UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,27 @@
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers;
 
-public class UsersController
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
 {
-    
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user = context.Users.Find(id);
+        
+        if (user == null) return NotFound();
+
+        return user;
+    }
 }
--- obj/Debug/net9.0/API.AssemblyInfo.cs
+++ obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e95e0acd7f9dec986f24b13ee29a5f574dd176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645bf8b7ef1cb06b4afcf9884902842ea32bc99a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
